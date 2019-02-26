57561b12f4bbe3602651c6add2da3d5325f97481
Sabbir Hossaain <sabbir.hossain2668@gmail.com>
2019-02-26 23:28:06 +0600

    layout added

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
+
+
+mix.styles([
+    'resources/assets/css/sb-admin-2.css',
+    'resources/assets/css/bootstrap-datatable/dataTables.bootstrap4.css',
+    'resources/assets/css/font-awesome/font-awesome.css'
+
+], 'public/css/libs.css');
+
+mix.scripts([
+    'resources/assets/js/jquery/jquery.js',
+    'resources/assets/js/jquery/jquery.easing.js',
+    'resources/assets/js/bootstrap/bootstrap.bundle.js',    
+    'resources/assets/js/bootstrap-datatable/jquery.dataTables.js'
+
+], 'public/js/libs.js');
